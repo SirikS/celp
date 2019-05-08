bf347869b4654abbb7b8d6d86b29d6d94a5fa272
TjerkHendriksen <tjerk.hendriksen@hotmail.nl>
2019-05-08 10:53:17 +0200

    Series met steden en aantal reviews

--- recommender.py
+++ recommender.py
@@ -1,6 +1,7 @@
 from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
 
 import random
+import pandas as pd
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
@@ -15,6 +16,26 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
             adress:str
         }
     """
+    if user_id:
+        plaatsen = []
+        for stad in CITIES:
+            for user in USERS[stad]:
+                if user['user_id'] == user_id:
+
+                    plaatsen.append(stad)
+        print(plaatsen) 
+        counts = []
+        for stad in plaatsen:
+            count = 0
+            for review in REVIEWS[stad]:
+                if review['user_id'] == user_id:
+                    count += 1
+            counts.append(count)
+        reviews_plaats = pd.Series(index=plaatsen, data=counts)
+        print(reviews_plaats)  
+
     if not city:
         city = random.choice(CITIES)
+        
+            
     return random.sample(BUSINESSES[city], n)
