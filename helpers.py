30466337f7400c76669df4551dfba610712bbab4
MatthijsVollenbroek <matthijs.vollenbroek@gmail.com>
2019-05-14 10:22:31 +0200

    helpers functie voor steden

---
+++ helpers.py
@@ -0,0 +1,24 @@
+from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
+import random
+import pandas as pd
+
+
+def location(userid):
+    # lijst aanmaken waar alle plaatsen van user inkomen
+    plaatsen = []
+    # per stad kijken of user in stad heeft gereviewt
+    for stad in CITIES:
+        for user in USERS[stad]:
+            if user['user_id'] == userid:
+                plaatsen.append(stad)
+    # lijst met aantal reviews per plaats door user            
+    counts = []
+    for stad in plaatsen:
+        count = 0
+        for review in REVIEWS[stad]:
+            if review['user_id'] == userid:
+                count += 1
+        counts.append(count)
+    # series aanmaken waarin het aantal reviews per plaats staan
+    reviews_plaats = pd.Series(index=plaatsen, data=counts)
+    return reviews_plaats  
--- recommender.py
+++ recommender.py
@@ -1,5 +1,5 @@
 from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
-
+import helpers
 import random
 import pandas as pd
 
@@ -23,24 +23,7 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
         
     # als user inlogt
     if user_id:
-        # lijst aanmaken waar alle plaatsen van user inkomen
-        plaatsen = []
-        # per stad kijken of user in stad heeft gereviewt
-        for stad in CITIES:
-            for user in USERS[stad]:
-                if user['user_id'] == user_id:
-                    plaatsen.append(stad)
-        # lijst met aantal reviews per plaats door user            
-        counts = []
-        for stad in plaatsen:
-            count = 0
-            for review in REVIEWS[stad]:
-                if review['user_id'] == user_id:
-                    count += 1
-            counts.append(count)
-        # series aanmaken waarin het aantal reviews per plaats staan
-        reviews_plaats = pd.Series(index=plaatsen, data=counts)
-        print(reviews_plaats)  
+        print(helpers.location(user_id))  
 
     if not city:
         city = random.choice(CITIES)
